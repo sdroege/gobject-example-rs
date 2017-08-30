4dfe5a814b3fd07a8b004c2451b20a3df592bb49
Sebastian Dröge <sebastian@centricular.com>
2017-08-30 11:54:05 +0300

    Use "incremented" signal in Python and Javascript example

--- test.js
+++ test.js
@@ -4,12 +4,20 @@ const Lang = imports.lang;
 const Ex = imports.gi.Ex;
 
 let foo = new Ex.Foo({name: "foo's name"});
+foo.connect("incremented", function(obj, val, inc) {
+    print("incremented to " + val + " by " + inc);
+});
+
 print("foo name: " + foo.get_name());
 print("foo inc 1: " + foo.increment(1));
 print("foo inc 10: " + foo.increment(10));
 print("foo counter: " + foo.get_counter());
 
 let bar = new Ex.Bar({name: "bar's name"});
+bar.connect("incremented", function(obj, val, inc) {
+    print("incremented to " + val + " by " + inc);
+});
+
 print("bar name: " + bar.get_name());
 print("bar inc 1: " + bar.increment(1));
 print("bar inc 10: " + bar.increment(10));
--- test.py
+++ test.py
@@ -4,13 +4,20 @@ import gi
 gi.require_version("Ex", "0.1")
 from gi.repository import Ex
 
+def on_incremented(obj, val, inc):
+    print("incremented to {} by {}".format(val, inc))
+
 foo = Ex.Foo.new("foo's name")
+foo.connect("incremented", on_incremented)
+
 print("foo name: " + str(foo.get_name()))
 print("foo inc 1: " + str(foo.increment(1)))
 print("foo inc 10: " + str(foo.increment(10)))
 print("foo counter: " + str(foo.get_counter()))
 
 bar = Ex.Bar.new("bar's name")
+bar.connect("incremented", on_incremented)
+
 print("bar name: " + str(bar.get_name()))
 print("bar inc 1: " + str(bar.increment(1)))
 print("bar inc 10: " + str(bar.increment(10)))
