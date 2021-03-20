7e1aa4d36c58d6fd389bbab314bf89a21d145c1f
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-03-20 20:04:27 +0400

    bar: untangle imp vs ffi modules
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- src/bar/ffi.rs
+++ src/bar/ffi.rs
@@ -2,19 +2,19 @@ use crate::foo;
 use libc::c_char;
 
 #[repr(C)]
-pub struct Bar {
+pub struct ExBar {
     pub parent: foo::ffi::ExFoo,
 }
 
 #[repr(C)]
-pub struct BarClass {
+pub struct ExBarClass {
     pub parent_class: foo::ffi::ExFooClass,
 }
 
 extern "C" {
-    pub fn ex_bar_new(name: *const c_char) -> *mut Bar;
+    pub fn ex_bar_new(name: *const c_char) -> *mut ExBar;
     pub fn ex_bar_get_type() -> glib::ffi::GType;
 
-    pub fn ex_bar_set_number(this: *mut Bar, num: f64);
-    pub fn ex_bar_get_number(this: *mut Bar) -> f64;
+    pub fn ex_bar_set_number(this: *mut ExBar, num: f64);
+    pub fn ex_bar_get_number(this: *mut ExBar) -> f64;
 }
