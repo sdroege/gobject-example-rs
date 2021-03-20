cc2396c0bcc35afc98334adba6c695adedc5f2fa
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-03-20 20:04:27 +0400

    nameable: untangle imp vs ffi modules
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- src/nameable/mod.rs
+++ src/nameable/mod.rs
@@ -1,22 +1,20 @@
 #[cfg(not(feature = "bindings"))]
 pub mod imp;
+#[cfg(not(feature = "bindings"))]
+use imp::ffi;
 
 #[cfg(feature = "bindings")]
 mod ffi;
-#[cfg(feature = "bindings")]
-pub mod imp {
-    pub use super::ffi::*;
-}
 
 use glib::subclass::prelude::*;
 use glib::translate::*;
 use glib::{Cast, IsA};
 
 glib::wrapper! {
-    pub struct Nameable(Interface<imp::Nameable, imp::NameableInterface>);
+    pub struct Nameable(Interface<ffi::ExNameable, ffi::ExNameableInterface>);
 
     match fn {
-        get_type => || imp::ex_nameable_get_type(),
+        get_type => || ffi::ex_nameable_get_type(),
     }
 }
 
@@ -26,7 +24,7 @@ pub trait NameableExt {
 
 impl<O: IsA<Nameable>> NameableExt for O {
     fn get_name(&self) -> Option<String> {
-        unsafe { from_glib_full(imp::ex_nameable_get_name(self.as_ref().to_glib_none().0)) }
+        unsafe { from_glib_full(ffi::ex_nameable_get_name(self.as_ref().to_glib_none().0)) }
     }
 }
 
@@ -43,7 +41,7 @@ unsafe impl<T: ObjectSubclass + NameableImpl> IsImplementable<T> for Nameable {
 }
 
 unsafe extern "C" fn get_name_trampoline<T: ObjectSubclass>(
-    nameable: *mut imp::Nameable,
+    nameable: *mut ffi::ExNameable,
 ) -> *mut libc::c_char
 where
     T: NameableImpl,
