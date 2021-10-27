2d007d8b0c7a141b01282deae21c04660f0e8678
Marc-André Lureau <marcandre.lureau@redhat.com>
2021-10-27 19:21:31 +0400

    Generate version macros
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
-use std::env;
 use cargo_metadata::*;
+use std::env;
 use std::path::*;
 
 use cbindgen::Builder;
@@ -12,7 +12,7 @@ fn main() {
         .exec()
         .unwrap();
 
-    println!("{:?}", meta);
+    println!("{:?}", &meta);
 
     if cfg!(feature = "bindings") {
         // assuming target dir isn't tweaked
@@ -25,16 +25,42 @@ fn main() {
     }
 
     if cfg!(feature = "capi") {
+        let version = &meta.root_package().unwrap().version;
+        let name = &meta.root_package().unwrap().metadata["capi"]["header"]["name"]
+            .as_str()
+            .unwrap();
         let out = std::env::var("OUT_DIR").unwrap();
         let out = Path::new(&out);
         let out_include = out.join("capi/include/");
         std::fs::create_dir_all(&out_include).unwrap();
 
+        let mut config = cbindgen::Config::default();
+        let warning = config.autogen_warning.unwrap_or_default();
+        let version_info = format!(
+            r"
+#define {0}_MAJOR_VERSION {1}
+#define {0}_MINOR_VERSION {2}
+#define {0}_PATCH_VERSION {3}
+
+#define {0}_CHECK_VERSION(major,minor,path)    \
+    ({0}_MAJOR_VERSION > (major) || \
+     ({0}_MAJOR_VERSION == (major) && {0}_MINOR_VERSION > (minor)) || \
+     ({0}_MAJOR_VERSION == (major) && {0}_MINOR_VERSION == (minor) && \
+      {0}_PATCH_VERSION >= (patch)))
+",
+            name.to_uppercase(),
+            version.major,
+            version.minor,
+            version.patch
+        );
+        config.autogen_warning = Some(warning + &version_info);
+
         Builder::new()
             .with_crate(&path)
+            .with_config(config)
             .with_gobject(true)
             .generate()
             .unwrap()
-            .write_to_file(out_include.join("ex.h"));
+            .write_to_file(out_include.join(format!("{}.h", name)));
     }
 }
